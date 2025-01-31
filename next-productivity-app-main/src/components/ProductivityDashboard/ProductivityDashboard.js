76858cefb87cc399515878afe0f32e281e82e22b
Kevin <madrid@max11>
2025-01-31 10:32:02 +0100

    Antes merge

--- next-productivity-app-main/src/components/ProductivityDashboard/ProductivityDashboard.js
+++ next-productivity-app-main/src/components/ProductivityDashboard/ProductivityDashboard.js
@@ -156,7 +156,7 @@ const ProductivityDashboard = (props) => {
     <div className="app">
       <div className="task-edit">
         <div className="task-form">
-        <p>Sludo</p>
+        <p>Saludo</p>
           <input
             type="text"
             placeholder="Título de la tarea"
